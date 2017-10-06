ee34e23e7fbe04515422095cd2dcafb041a76ccb
Evgeny Zhernoklev <evgenyzhernoklev@MacBook-Pro-Evgeny.local>
2017-10-06 11:59:02 +0300

    Update header & footer, add persons page

--- wp-content/themes/twentysixteen/footer.php
+++ wp-content/themes/twentysixteen/footer.php
@@ -12,16 +12,8 @@
 
 	<footer class="footer" role="contentinfo">
 		<div class="contentIn">
-			<?php if ( has_nav_menu( 'primary' ) ) : ?>
-				<nav class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Primary Menu', 'twentysixteen' ); ?>">
-					<?php
-						wp_nav_menu( array(
-							'theme_location' => 'primary',
-							'menu_class'     => 'primary-menu',
-						 ) );
-					?>
-				</nav><!-- .main-navigation -->
-			<?php endif; ?>
+
+			<?php twentysixteen_the_custom_logo(); ?>
 
 			<?php if ( has_nav_menu( 'social' ) ) : ?>
 				<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'twentysixteen' ); ?>">
@@ -34,21 +26,9 @@
 							'link_after'     => '</span>',
 						) );
 					?>
-				</nav><!-- .social-navigation -->
+				</nav>
 			<?php endif; ?>
 
-			<div class="site-info">
-				<?php
-					/**
-					 * Fires before the twentysixteen footer text for footer customization.
-					 *
-					 * @since Twenty Sixteen 1.0
-					 */
-					do_action( 'twentysixteen_credits' );
-				?>
-				<span class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span>
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentysixteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentysixteen' ), 'WordPress' ); ?></a>
-			</div><!-- .site-info -->
 		</div>
 	</footer>
 
---
+++ wp-content/themes/twentysixteen/person-page.php
@@ -0,0 +1,65 @@
+<?php
+/*
+Template Name: Шаблон страницы персон
+*/
+?>
+
+<?php get_header(); ?>
+
+<main class="contentIn" role="main">
+	<?php the_title( '<h1 class="titlePage">', '</h1>' ); ?>
+
+	<?php
+	// запрос
+	$wpb_all_query = new WP_Query(array(
+																	'post_type'				=>'person',
+																	'post_status'			=>'publish',
+																	'posts_per_page'	=> -1,
+																	'order' 					=> 'ASC',
+																	'orderby' 				=> 'menu_order'
+																)); ?>
+
+	<?php if ( $wpb_all_query->have_posts() ) : ?>
+
+		<div class="colsFlex colsFlex--2 colsFlex--center">
+			<?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
+
+				<?php $link = get_post_meta($post->ID, '_person_link_meta_key', true); ?>
+
+				<div class="colsFlex__col colsFlex__col--2">
+					<div class="partner">
+
+						<?php if ( $link ) : ?>
+							<div class="partnerImg">
+								<a class="partnerImg__link" href="<?php echo $link ?>" target="_blank">
+									<?php the_post_thumbnail(); ?>
+								</a>
+							</div>
+							<h2 class="partnerTitle"><a class="partnerTitle__link"
+																					href="<?php echo $link ?>"
+																					target="_blank"><?php the_title(); ?></a></h2>
+						<?php else : ?>
+							<div class="partnerImg">
+								<?php the_post_thumbnail(); ?>
+							</div>
+							<h2 class="partnerTitle"><?php the_title(); ?></h2>
+						<?php endif; ?>
+
+						<div class="partnerInfo">
+							<?php the_excerpt(); ?>
+						</div>
+					</div>
+				</div>
+
+			<?php endwhile; ?>
+		</div>
+
+		<?php wp_reset_postdata(); ?>
+
+	<?php else : ?>
+		<p><?php _e( 'Извините, нет записей, соответствуюших Вашему запросу.' ); ?></p>
+	<?php endif; ?>
+
+</main>
+
+<?php get_footer(); ?>
