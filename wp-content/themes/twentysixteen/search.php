e3243d8eefd98f90178dbcd1e968a00c5ae19a52
evgenyzhernoklev <oldschool127@mail.ru>
2017-11-11 21:21:05 +0300

    Add navigation in search results, update translations

--- wp-content/themes/twentysixteen/search.php
+++ wp-content/themes/twentysixteen/search.php
@@ -30,12 +30,7 @@ get_header(); ?>
 		// End the loop.
 		endwhile;
 
-		// Previous/next page navigation.
-		the_posts_pagination( array(
-			'prev_text'          => __( 'Previous page', 'twentysixteen' ),
-			'next_text'          => __( 'Next page', 'twentysixteen' ),
-			'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>',
-		) );
+		wp_pagenavi();
 
 	// If no content, include the "No posts found" template.
 	else :
