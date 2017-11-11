614d4af7576a9f41514ebb859d0201ad0c41cc53
evgenyzhernoklev <oldschool127@mail.ru>
2017-11-11 18:13:23 +0300

    Update search results

--- wp-content/themes/twentysixteen/template-parts/content-search.php
+++ wp-content/themes/twentysixteen/template-parts/content-search.php
@@ -10,44 +10,50 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-	</header><!-- .entry-header -->
+		<?php the_title( sprintf( '<h2 class="entry-title searchResults__title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
-	<?php twentysixteen_post_thumbnail(); ?>
+		<p class="searchResults__date"><?php echo get_the_date('F j, Y'); ?></p>
+	</header>
 
-	<?php twentysixteen_excerpt(); ?>
+	<div class="searchResults__imgWrapper">
+		<?php twentysixteen_post_thumbnail(); ?>
+	</div>
+
+	<div class="searchResults__info">
+		<?php twentysixteen_excerpt(); ?>
+	</div>
+
 
 	<?php if ( 'post' === get_post_type() ) : ?>
 
-		<footer class="entry-footer">
-			<?php twentysixteen_entry_meta(); ?>
+		<footer class="searchResults__footer entry-footer">
 			<?php
-				edit_post_link(
-					sprintf(
-						/* translators: %s: Name of current post */
-						__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
-						get_the_title()
-					),
-					'<span class="edit-link">',
-					'</span>'
-				);
+				// twentysixteen_entry_meta();
+				$posttags = get_the_tags();
+				if ($posttags) {
+					echo '<div class="searchResults__meta"><span class="searchResults__metaTitle">Метки:</span>';
+					foreach($posttags as $tag) {
+						$name = $tag->name;
+						$link = get_tag_link($tag->term_id);
+						echo '<a class="" href="' . $link . '">' . $name . '</a>';
+					}
+					echo '</div>';
+				}
+
+				$categories = get_the_category($post_id);
+				if ($categories) {
+					echo '<div class="searchResults__meta"><span class="searchResults__metaTitle">Категории:</span>';
+					foreach ($categories as $category) {
+						echo '<a href="'. get_category_link($category->term_id) . '">' . $category->cat_name . '</a>';
+					}
+					echo '</div>';
+				}
 			?>
-		</footer><!-- .entry-footer -->
+		</footer>
 
 	<?php else : ?>
 
-		<?php
-			edit_post_link(
-				sprintf(
-					/* translators: %s: Name of current post */
-					__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
-					get_the_title()
-				),
-				'<footer class="entry-footer"><span class="edit-link">',
-				'</span></footer><!-- .entry-footer -->'
-			);
-		?>
+
 
 	<?php endif; ?>
-</article><!-- #post-## -->
-
+</article>
