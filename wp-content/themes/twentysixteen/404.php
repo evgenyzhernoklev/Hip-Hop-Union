ef2d5ccb60fdb89bed3fdd6fcd676c33dac36015
evgenyzhernoklev <oldschool127@mail.ru>
2018-04-26 10:49:51 +0300

    Update styles for 404 page

--- wp-content/themes/twentysixteen/404.php
+++ wp-content/themes/twentysixteen/404.php
@@ -9,26 +9,15 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentysixteen' ); ?></h1>
-				</header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentysixteen' ); ?></p>
+<main class="contentIn contentIn--small" role="main">
+	<h1 class="titlePage">Страница не найдена</h1>
 
-					<?php get_search_form(); ?>
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
+	<div class="entry-content searchContent">
+		<img class="searchContent__imgError" src="/wp-content/uploads/icons/404.svg" />
+		<?php get_search_form(); ?>
+	</div>
+</main>
 
-		</main><!-- .site-main -->
-
-		<?php get_sidebar( 'content-bottom' ); ?>
-
-	</div><!-- .content-area -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
