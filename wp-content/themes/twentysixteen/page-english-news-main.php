d702a763d631921c26800edf6dda17715cecead8
evgenyzhernoklev <oldschool127@mail.ru>
2018-04-27 12:59:30 +0300

    Add fixes

--- wp-content/themes/twentysixteen/page-english-news-main.php
+++ wp-content/themes/twentysixteen/page-english-news-main.php
@@ -13,7 +13,7 @@ get_header(); ?>
 																	'post_type'				=>'news',
 																	'category_name'		=> 'Main news',
 																	'post_status'			=>'publish',
-																	'posts_per_page'	=> 1
+																	'posts_per_page'	=> -1
 																)); ?>
 
 	<?php if ( $wpb_all_query->have_posts() ) : ?>
@@ -26,8 +26,6 @@ get_header(); ?>
 				endwhile;
 			?>
 		</div>
-
-		<?php wp_pagenavi( array( 'query' => $wpb_all_query ) ); ?>
 
 		<?php wp_reset_postdata(); ?>
 
